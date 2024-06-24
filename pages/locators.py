c01dd017211a7c53f2f42d570db2e3a9e0e6fe90
Lina-Humphris <postcrossing2002@mail.ru>
2024-06-24 12:12:37 +0300

    Looking for a bug in 10 links

--- pages/locators.py
+++ pages/locators.py
@@ -10,8 +10,8 @@ class LoginPageLocators():
 
 class ProductPageLocators():
     ADD_TO_BASKET = (By.CSS_SELECTOR, "button.btn-add-to-basket")
-    NAME_OF_BOOK = (By.CSS_SELECTOR, "div.alert-success")
+    NAME_OF_BOOK = (By.CSS_SELECTOR, "div.alert-success div strong")
     NAME_OF_BOOK_RIGHT = (By.CSS_SELECTOR, "div h1")
-    COST_OF_BASKET = (By.CSS_SELECTOR, "div.alert-info")
+    COST_OF_BASKET = (By.CSS_SELECTOR, "div.alert-info p strong")
     COST_OF_BASKET_RIGHT = (By.CSS_SELECTOR, "div.col-sm-6 p.price_color")
     
--- pages/product_page.py
+++ pages/product_page.py
@@ -14,9 +14,9 @@ class ProductPage(BasePage):
     def should_be_right_name_of_book(self):
         name_of_book_right = self.browser.find_element(*ProductPageLocators.NAME_OF_BOOK_RIGHT)
         name_of_book = self.browser.find_element(*ProductPageLocators.NAME_OF_BOOK)
-        assert name_of_book_right.text in name_of_book.text, name_of_book.text
+        assert name_of_book_right.text == name_of_book.text, name_of_book.text
     
     def basket_cost(self):
         cost_of_basket_right = self.browser.find_element(*ProductPageLocators.COST_OF_BASKET_RIGHT)
         cost_of_basket = self.browser.find_element(*ProductPageLocators.COST_OF_BASKET)
-        assert cost_of_basket_right.text in cost_of_basket.text, cost_of_basket.text
+        assert cost_of_basket_right.text == cost_of_basket.text, cost_of_basket.text
